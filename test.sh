690aba7c0971d4397406afc59c689c0927b69fc7
Chris Kimmel <kimmel.95@osu.edu>
2020-06-25 10:52:33 -0400

    Make the test work

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@ test_prs=/fs/project/PAS1405/General/Kimmel_Chris/061120_8079m6A_IVTcarrierRNA_l
 
 conda activate tombo
 echo "Python 3 test"
-python3 PRS_converter.py -op $test_prs out.csv
+python3 PRS_converter.py -op $test_prs out_py2.csv
 
 conda activate tombo2
 echo "Python 2 test"
-python PRS_converter.py -op $test_prs out.csv
+python PRS_converter.py -op $test_prs out_py3.csv
 
